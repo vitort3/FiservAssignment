6717e42a845823733a90c766f66ef1511ff15b09
Vitor Augusto Silva <vitort3@gmail.com>
2025-12-08 16:56:05 -0300

    initial tests

--- FiservAssignmentTests/FiservAssignmentTests.swift
+++ FiservAssignmentTests/FiservAssignmentTests.swift
@@ -10,27 +10,20 @@ import XCTest
 
 final class FiservAssignmentTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // AccountListViewModel tests
+    func testLoadAccounts() {
+        
     }
+    func testGetAccountAtIndexPath() {}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    // AccountDetailsViewModel tests
+    func testLoadAccountDetailsAndTransactions() {}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    // AccountServiceProtocol tests
+    func testFetchAccounts() {}
+    func testFetchAccountDetails() {}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    // TransactionServiceProtocol tests
+    func testFetchTransactions() {}
 
 }
